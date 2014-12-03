9aa92e5f5381d3b43dc5419c70447c106c068390
Anton87 <anton_987@libero.it>
2014-12-03 11:24:36 +0100

    Added paragraphs segmenter for candidate answers

--- src/it/unitn/nlpir/itwiki/ssplitter/WikiParagraphsSegmenter.java
+++ src/it/unitn/nlpir/itwiki/ssplitter/WikiParagraphsSegmenter.java
@@ -2,7 +2,6 @@ package it.unitn.nlpir.itwiki.ssplitter;
 
 import static org.apache.uima.fit.factory.AnalysisEngineFactory.createEngine;
 import it.unitn.nlpir.italian.textpro.TextProSegmenter;
-import it.unitn.nlpir.wiki.CandidateGenerator;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -10,6 +9,7 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Date;
 
 import org.apache.commons.cli.BasicParser;
 import org.apache.commons.cli.CommandLine;
@@ -41,11 +41,14 @@ import de.tudarmstadt.ukp.dkpro.core.opennlp.OpenNlpSegmenter;
 public class WikiParagraphsSegmenter {
 
 	private static final String FS = "\t"; // fields separator
+	/* print a log msg each tot sentences */
+	private static final int SENTENCES_NUM_PER_LOG = 10000;
+	private static final String SENTENCES_NUM_PER_LOG_OPT = "sentencesNumPerLog";
 
 	private static final String SEGMENTER_OPT = "segmenter";
 	private static final String LANGUAGE_OPT = "lang";
 	private static final String WIKI_CORPUS_PATH_OPT = "wikiFile";
-	private static final String WIKI_CORPUS_SEGMENTED_PATH = "outputWikiFile";
+	private static final String WIKI_CORPUS_SEGMENTED_PATH_OPT = "outputWikiFile";
 
 	public static void main(String[] args) {
 		Options options = new Options();
@@ -56,8 +59,10 @@ public class WikiParagraphsSegmenter {
 				"The text language (it or en)");
 		options.addOption(WIKI_CORPUS_PATH_OPT, true,
 				"The path of the file containing wikipedia corpus");
-		options.addOption(WIKI_CORPUS_SEGMENTED_PATH, true, 
+		options.addOption(WIKI_CORPUS_SEGMENTED_PATH_OPT, true, 
 				"The path where to save the wikipedia segmented corpus");
+		options.addOption(SENTENCES_NUM_PER_LOG_OPT, true, 
+				"The number of sentences to process before printing a log message");
 
 		CommandLineParser parser = new BasicParser();
 		try {
@@ -97,17 +102,24 @@ public class WikiParagraphsSegmenter {
 			}
 
 			// Get the output path where to save the segmented it-wiki corpus
-			String outputWikiFilePath = cmd.getOptionValue(WIKI_CORPUS_SEGMENTED_PATH);
+			String outputWikiFilePath = cmd.getOptionValue(WIKI_CORPUS_SEGMENTED_PATH_OPT);
 			if (outputWikiFilePath == null || outputWikiFilePath.trim().equals("")) {
 				System.err.println("outputWikiFile not specified");
 				System.exit(1);
+			}
+			
+			int sentencesNumPerLog = SENTENCES_NUM_PER_LOG;
+			String sentencesNumPerLogValue = cmd.getOptionValue(SENTENCES_NUM_PER_LOG_OPT);
+			if (sentencesNumPerLogValue != null && !sentencesNumPerLogValue.trim().equals("")) {
+				sentencesNumPerLog = Integer.parseInt(sentencesNumPerLogValue);
 			}
 			
 			System.out.println(WikiParagraphsSegmenter.class.getSimpleName() + " [");
 			System.out.println("    " + LANGUAGE_OPT + ":  " + language);
 			System.out.println("    " + SEGMENTER_OPT + ": " + segmenterName);
 			System.out.println("    " + WIKI_CORPUS_PATH_OPT + ": " + wikiFilePath);
-			System.out.println("    " + WIKI_CORPUS_SEGMENTED_PATH + ": " + outputWikiFilePath) ;
+			System.out.println("    " + WIKI_CORPUS_SEGMENTED_PATH_OPT + ": " + outputWikiFilePath) ;
+			System.out.println("    " + SENTENCES_NUM_PER_LOG_OPT +  ": " + sentencesNumPerLog);
 			System.out.println("]");
 
 			AnalysisEngine ssegmenter = null;
@@ -132,16 +144,20 @@ public class WikiParagraphsSegmenter {
 				System.err.println("Wrong segmenter paramater. The value can be 'TextPro' or 'OpenNLP'.");
 				System.exit(1);
 			}
-
+			
+			int totalLinesNum = 0;
 			PrintWriter out = 
 					new PrintWriter(
 							new FileWriter(outputWikiFilePath));
+			String line = null;
 			try (BufferedReader in = 
 					new BufferedReader(
-							new FileReader(wikiFilePath))) { 
-
+							new FileReader(wikiFilePath))) {
+				
+				Date start = new Date(); // record start date
+				
 				JCas jcas = JCasFactory.createJCas();
-				for (String line = in.readLine(); line != null; line = in.readLine()) { 
+				for (line = in.readLine(); line != null; line = in.readLine()) { 
 					String[] linesplit = line.split(FS);					
 					if (linesplit.length != 3) {
 						throw new IOException("Wrong format error: expected 3 fields, got " + linesplit.length);
@@ -158,14 +174,28 @@ public class WikiParagraphsSegmenter {
 					for (Sentence sentence : JCasUtil.select(jcas, Sentence.class)) {
 						String sid = pid + ":" + sentenceNum++; // sentence id
 						out.println(sid + FS + sentence.getCoveredText() + FS + linesplit[2]);
-					}					
-				}
-			} finally {
+					}		
+					totalLinesNum++;
+					if (totalLinesNum % sentencesNumPerLog == 0) {
+						Date now = new Date();
+						System.out.printf("Processed %d lines from file: %s... (%d total milliseconds elapsed) \n", 
+								totalLinesNum, wikiFilePath, now.getTime() - start.getTime());
+						out.flush();
+					}
+				}				
+				Date end = new Date();
+				System.out.println(end.getTime() - start.getTime() + " total milliseconds elapsed");
+			} catch (UIMAException e) { 
+				System.err.println("Error while processing line num. " + totalLinesNum + " ... ");
+				System.err.println(line);
+				System.err.println(totalLinesNum + 1);
+			}
+				finally {
 				out.close();
 			}
 		} catch (ParseException e) {
 			e.printStackTrace();
-		} catch (UIMAException e) { 
+		} catch (UIMAException e) {
 			e.printStackTrace();
 		} catch (IOException e) {
 			e.printStackTrace();
